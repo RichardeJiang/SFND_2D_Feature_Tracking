9e9d545a26c9b9f5f258779b29430e628d060ad3
Richard Jiang <richardejiang@hotmail.com>
2021-11-12 23:29:05 -0500

    fack this project

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -101,3 +101,108 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         cv::waitKey(0);
     }
 }
+
+// Harris, assuming the passed in img is alr in gray scale
+void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis) {
+
+    // Param from original Harris exercise
+    int blockSize = 2;
+    int apertureSize = 3;
+    int minResponse = 100;
+    double k = 0.04;
+
+    double t = (double)cv::getTickCount();
+
+    // Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // vector<cv::KeyPoint> keypoints;
+    double maxOverlap = 0.0;
+    for (int r = 0; r < dst_norm.rows; r++)
+    {
+        for (int c = 0; c < dst_norm.cols; c++)
+        {
+            int response = (int)dst_norm.at<float>(r, c);
+            if (response > minResponse)
+            {
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(c, r);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {
+                            *it = newKeyPoint;
+                            break;
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     
+
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "Harris detection with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    // visualize results
+    if (bVis)
+    {
+        cv::Mat visImage = img.clone();
+        cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        string windowName = "Harris Corner Detector Results";
+        cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
+
+// All other detectors: FAST, BRISK, ORB, AKAZE, and SIFT
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis) {
+    double t = (double)cv::getTickCount();
+    if (detectorType.compare("FAST") == 0) {
+
+    } else if (detectorType.compare("BRISK") == 0) {
+        // threshold, octaves, patternScale
+        cv::BRISK briskDetector(60, 4, 1.0f);
+        briskDetector.create("Feature2D.BRISK");
+        briskDetector.detect(img, keypoints);
+    } else if (detectorType.compare("ORB") == 0) {
+
+    } else if (detectorType.compare("AKAZE") == 0) {
+
+    } else if (detectorType.compare("SIFT") == 0) {
+
+    } // assuming the input will always be correct, so ignore the else statement
+    
+    //detector->detect(img, keypoints);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << detectorType << " detector with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    string windowName = detectorType + " Detector Results";
+
+    // visualize results
+    if (bVis)
+    {
+        cv::Mat visImage = img.clone();
+        cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
